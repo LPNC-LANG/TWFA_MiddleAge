9786b7f4679d33b4dccaa29e86bd834b58784a6a
LPNC-LANG <130466949+LPNC-LANG@users.noreply.github.com>
2023-12-22 13:28:53 +0100

    Add files via upload

---
+++ DWI/TWI/code/gen_representative_sample.R
@@ -0,0 +1,59 @@
+library(rio)
+library(tidyverse)
+
+set.seed(1551)
+
+df <- rio::import("C:/Users/Clement/Nextcloud/THESE CLEMENT GUICHET/SCFC/participant_data_DTI.csv")
+
+# save mean and sd the subsamples should have
+aimed_mean <- mean(df$age)
+aimed_sd <- sd(df$age)
+
+# set number of replications / iterations
+n_replication <- 10000
+
+# set size of sample
+size_sample <- 35
+
+# set desired number of samples
+n_sample <- 1
+
+# set deviation from mean and sd you can accept
+deviation_mean <- .01*aimed_mean
+deviation_sd <- .01*aimed_sd
+
+# create empty container for resulting samples
+samples <- list(n_replication)
+
+# Repeatedly sample from length
+i <- 0
+sample_count <- 0
+
+repeat {
+  
+  i <- i+1
+  
+  # take a sample from length
+  sample_obs <- as.data.frame(sample(df$Observations, size_sample, replace = F)) %>% dplyr::rename(Observations = `sample(df$Observations, size_sample, replace = F)`)
+  sample_length <- left_join(sample_obs, df, by = "Observations")
+  
+  
+  # keep the sample when is is close enough
+  if(abs(aimed_mean - mean(sample_length$age)) < deviation_mean &
+     abs(aimed_sd - sd(sample_length$age)) < deviation_sd){
+    
+    samples[[i]] <- sample_length$Observations
+    sample_count <- sample_count + 1
+    
+  }
+  
+  if(i == n_replication | sample_count == n_sample){
+    break
+  }
+  
+}
+
+# your samples
+sampled_df <- unlist(samples) %>% as.data.frame()
+write_tsv(sampled_df, "template_sample.txt")
+
